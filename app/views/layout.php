b63ec9352964e23b23c6939c9ff52f54d2019b7f
InmaAranda <inmaaranda77@gmail.com>
2025-09-05 17:45:04 +0200

    Creadas la vista del registro de usuario y layout
    Se ha creado el formulario de registro y el layout principal
    de la página web

---
+++ app/views/layout.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TechZone - Tienda de infomática</title> <!--Esta es la plantilla de la web, la vista. por lo que es importante rellenar todo el esqueleto para no repetirlo todo el rato-->
+    <link rel="stylesheet" href="../../public/assests/style.css"> <!--tienes que coger y salir de la ruta y entrar en public así-->
+</head>
+<body>
+    <header>
+
+        <h1>TechZone - Tu tienda de informática  </h1>
+
+            <nav>
+                <ul>
+                    <li><a href="/">Inicio</a></li> <!--Se utiliza en href= / porque de esta manera se busca el index-->
+                </ul>
+            </nav>
+
+        <div class="botones-login">
+            <a href="/registro">Login</a>
+        </div>
+
+    </header>
+
+    <main>
+        <?php include($vista); ?>
+    </main>
+
+    <footer>
+        <p>$copy; <?=date('Y') ?> TechZone. Todos los derechos reservados. </p>
+    </footer>
+</body>
+</html>
